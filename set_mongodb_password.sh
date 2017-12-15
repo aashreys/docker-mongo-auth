073d7fbc7e5c83600175ae96981daedff1275e22
David Anaya <danaya@gmail.com>
2017-12-15 15:31:23 +0100

    Add support for the mvp service in the mongo auth configuration

--- set_mongodb_password.sh
+++ set_mongodb_password.sh
@@ -4,39 +4,45 @@
 MONGODB_ADMIN_USER=${MONGODB_ADMIN_USER:-"admin"}
 MONGODB_ADMIN_PASS=${MONGODB_ADMIN_PASS:-"4dmInP4ssw0rd"}
 
-# Application Database User
-MONGODB_APPLICATION_DATABASE=${MONGODB_APPLICATION_DATABASE:-"admin"}
-MONGODB_APPLICATION_USER=${MONGODB_APPLICATION_USER:-"restapiuser"}
-MONGODB_APPLICATION_PASS=${MONGODB_APPLICATION_PASS:-"r3sT4pIp4ssw0rd"}
+create_user() {
+    MONGODB_APPLICATION_DATABASE=${1}
+    MONGODB_APPLICATION_USER=${2}
+    MONGODB_APPLICATION_PASS=${3}
+    # Wait for MongoDB to boot
+    RET=1
+    while [[ RET -ne 0 ]]; do
+        echo "=> Waiting for confirmation of MongoDB service startup..."
+        sleep 5
+        mongo admin --eval "help" >/dev/null 2>&1
+        RET=$?
+    done
 
-# Wait for MongoDB to boot
-RET=1
-while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MongoDB service startup..."
-    sleep 5
-    mongo admin --eval "help" >/dev/null 2>&1
-    RET=$?
-done
+    # Create the admin user
+    echo "=> Creating admin user with a password in MongoDB"
+    mongo admin --eval "db.createUser({user: '$MONGODB_ADMIN_USER', pwd: '$MONGODB_ADMIN_PASS', roles:[{role:'root',db:'admin'}]});"
 
-# Create the admin user
-echo "=> Creating admin user with a password in MongoDB"
-mongo admin --eval "db.createUser({user: '$MONGODB_ADMIN_USER', pwd: '$MONGODB_ADMIN_PASS', roles:[{role:'root',db:'admin'}]});"
+    sleep 3
 
-sleep 3
-
-# If we've defined the MONGODB_APPLICATION_DATABASE environment variable and it's a different database
-# than admin, then create the user for that database.
-# First it authenticates to Mongo using the admin user it created above.
-# Then it switches to the REST API database and runs the createUser command 
-# to actually create the user and assign it to the database.
-if [ "$MONGODB_APPLICATION_DATABASE" != "admin" ]; then
-    echo "=> Creating a ${MONGODB_APPLICATION_DATABASE} database user with a password in MongoDB"
-    mongo admin -u $MONGODB_ADMIN_USER -p $MONGODB_ADMIN_PASS << EOF
-echo "Using $MONGODB_APPLICATION_DATABASE database"
-use $MONGODB_APPLICATION_DATABASE
-db.createUser({user: '$MONGODB_APPLICATION_USER', pwd: '$MONGODB_APPLICATION_PASS', roles:[{role:'dbOwner', db:'$MONGODB_APPLICATION_DATABASE'}]})
+    # If we've defined the MONGODB_APPLICATION_DATABASE environment variable and it's a different database
+    # than admin, then create the user for that database.
+    # First it authenticates to Mongo using the admin user it created above.
+    # Then it switches to the REST API database and runs the createUser command 
+    # to actually create the user and assign it to the database.
+    if [ "$MONGODB_APPLICATION_DATABASE" != "admin" ]; then
+        echo "=> Creating a ${MONGODB_APPLICATION_DATABASE} database user with a password in MongoDB"
+        mongo admin -u $MONGODB_ADMIN_USER -p $MONGODB_ADMIN_PASS << EOF
+    echo "Using $MONGODB_APPLICATION_DATABASE database"
+    use $MONGODB_APPLICATION_DATABASE
+    db.createUser({user: '$MONGODB_APPLICATION_USER', pwd: '$MONGODB_APPLICATION_PASS', roles:[{role:'dbOwner', db:'$MONGODB_APPLICATION_DATABASE'}]})
 EOF
-fi
+    fi
+}
+
+# Reflections Database User
+create_user ${MONGODB_REFLECTIONS_DATABASE:-"reflections"} ${MONGODB_REFLECTIONS_USER:-"reflectionsAppUser"} ${MONGODB_REFLECTIONS_PASS:-"reflectionsPass"}
+
+# Mvp Database User
+create_user ${MONGODB_MVP_DATABASE:-"mvp"} ${MONGODB_MVP_USER:-"mvpAppUser"} ${MONGODB_MVP_PASS:-"mvpPass"}
 
 sleep 1
 
